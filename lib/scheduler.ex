c100fd09d5c76e92a90122a345641cc5b7421711
Nate Black <nathanael.black@gmail.com>
2018-08-30 13:02:22 -0700

    add division scheduler. still needs to repeat for x weeks

--- lib/scheduler.ex
+++ lib/scheduler.ex
@@ -2,6 +2,55 @@ defmodule Scheduler do
 
   require Integer
 
+
+  @doc """
+
+  Generates a schedule for a list of divisions
+
+    iex> Scheduler.schedule_for_divisions([[1, 2], [3, 4]])
+    [
+      [{1, 2}, {3, 4}],
+      [{1, 3}, {2, 4}],
+      [{1, 4}, {2, 3}]
+    ]
+
+    iex> Scheduler.schedule_for_divisions([[1, 2], [3, 4], [5, 6]])
+    [
+      [{1, 2}, {3, 4}, {5, 6}],
+      [{1, 3}, {2, 5}, {4, 6}],
+      [{1, 4}, {2, 6}, {3, 5}],
+      [{1, 5}, {2, 4}, {3, 6}],
+      [{1, 6}, {2, 3}, {4, 5}]
+    ]
+
+    iex> Scheduler.schedule_for_divisions([[1, 2, 3, 4], [5, 6, 7, 8]])
+    [
+      [{1, 2}, {3, 4}, {5, 6}, {7, 8}],
+      [{1, 3}, {2, 4}, {5, 7}, {6, 8}],
+      [{1, 4}, {2, 3}, {5, 8}, {6, 7}],
+      [{1, 5}, {2, 6}, {3, 7}, {4, 8}],
+      [{1, 6}, {2, 5}, {3, 8}, {4, 7}],
+      [{1, 7}, {2, 8}, {3, 5}, {4, 6}],
+      [{1, 8}, {2, 7}, {3, 6}, {4, 5}]
+    ]
+
+  """
+  def schedule_for_divisions(divisions) do
+    division_schedule = divisions |> Enum.map(&valid_schedules/1)
+                                  |> Enum.zip
+                                  |> Enum.map(&Tuple.to_list/1)
+                                  |> Enum.map(&Enum.concat/1)
+
+    division_matchups_to_exclude = Enum.concat(division_schedule)
+                                   |> MapSet.new
+
+    interdivision_schedule = valid_schedules(Enum.concat(divisions), division_matchups_to_exclude)
+
+    Enum.concat(division_schedule, interdivision_schedule)
+
+    # for inter-division play, exclude division matchups by putting them in the matchups_used filter
+  end
+
   @moduledoc """
   Documentation for Scheduler.
   """
@@ -22,28 +71,30 @@ defmodule Scheduler do
     [[{"bye", 1}, {2, 3}], [{"bye", 2}, {1, 3}], [{"bye", 3}, {1, 2}]]
 
   """
-  def valid_schedules(rosters) do
+  def valid_schedules(rosters, exclude_matchups \\ MapSet.new()) do
     possible_weeks = available_matchup_sets(rosters)
 
-    _valid_schedules(MapSet.new(), possible_weeks)
+    _valid_schedules(exclude_matchups, possible_weeks)
   end
 
   def _valid_schedules(_, []) do
     []
   end
 
-  def _valid_schedules(matchups_used, [week | other_possible_weeks]) do
-    next_matchups_used = MapSet.new(week) |> MapSet.union(matchups_used)
-
-    valid_weeks_remaining = other_possible_weeks |> Enum.reject(fn other_week ->
-      Enum.any?(other_week, fn matchup ->
-        MapSet.member?(next_matchups_used, matchup)
+  def _valid_schedules(matchups_used, possible_weeks) do
+    valid_weeks = possible_weeks |> Enum.reject(fn week ->
+      Enum.any?(week, fn matchup ->
+        MapSet.member?(matchups_used, matchup)
       end)
     end)
 
 
-    # TODO: this is only one schedule, but there are other valid ones
-    [week | _valid_schedules(next_matchups_used, valid_weeks_remaining)]
+    case valid_weeks do
+      # TODO: this is only one schedule, but there are other valid ones
+      [week | remaining_weeks] ->
+        [week | _valid_schedules(MapSet.new(week) |> MapSet.union(matchups_used), remaining_weeks)]
+      [] -> []
+    end
 
   end
 
