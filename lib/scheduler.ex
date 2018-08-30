98012646cf2b568f92ebf5bd0974cf5925f9191d
Nate Black <nathanael.black@gmail.com>
2018-08-30 11:24:50 -0700

    added valid_schedules() generator, but now only makes 1 schedule

--- lib/scheduler.ex
+++ lib/scheduler.ex
@@ -6,13 +6,50 @@ defmodule Scheduler do
   Documentation for Scheduler.
   """
 
+  @doc """
+
+  Generates a set of all valid complete schedules for the given rosters. Each schedule consists
+  of a list of weekly matchup sets. The number of weeks in the schedule depends on the number of
+  rosters but is equal to n = Enum.count(rosters) if n is odd or n - 1 if rosters is even. If n is odd,
+  each week will contain a bye.
+
+  ## Examples
+
+    iex> Scheduler.valid_schedules([1, 2])
+    [[{1, 2}]]
+
+    iex> Scheduler.valid_schedules([1, 2, 3])
+    [[{"bye", 1}, {2, 3}], [{"bye", 2}, {1, 3}], [{"bye", 3}, {1, 2}]]
+
+  """
+  def valid_schedules(rosters) do
+    possible_weeks = available_matchup_sets(rosters)
+
+    _valid_schedules(MapSet.new(), possible_weeks)
+  end
+
+  def _valid_schedules(_, []) do
+    []
+  end
+
+  def _valid_schedules(matchups_used, [week | other_possible_weeks]) do
+    next_matchups_used = MapSet.new(week) |> MapSet.union(matchups_used)
+
+    valid_weeks_remaining = other_possible_weeks |> Enum.reject(fn other_week ->
+      Enum.any?(other_week, fn matchup ->
+        MapSet.member?(next_matchups_used, matchup)
+      end)
+    end)
+
+
+    # TODO: this is only one schedule, but there are other valid ones
+    [week | _valid_schedules(next_matchups_used, valid_weeks_remaining)]
+
+  end
 
   @doc """
   Generates all legal matchup sets for a week/leg
-
-  A matchup set is a complete set of matchups where all rosters have a matchup.
-
-  If there are any fixed_matchups, those must be included in the set.
+  A matchup set is a complete week of matchups where all rosters have a matchup.
 
   ## Examples
 
@@ -21,6 +58,16 @@ defmodule Scheduler do
 
     iex> Scheduler.available_matchup_sets([1, 2, 3, 4])
     [[{1, 2}, {3, 4}], [{1, 3}, {2, 4}], [{1, 4}, {2, 3}]]
+
+    iex> Scheduler.available_matchup_sets([1, 2, 3])
+    [[{"bye", 1}, {2, 3}], [{"bye", 2}, {1, 3}], [{"bye", 3}, {1, 2}]]
+
+    iex> Scheduler.available_matchup_sets([1, 2, 3, 4, 5]) |> Enum.at(0)
+    [{"bye", 1}, {2, 3}, {4, 5}]
+
+    iex> Scheduler.available_matchup_sets([1, 2, 3, 4, 5]) |> Enum.at(1)
+    [{"bye", 1}, {2, 4}, {3, 5}]
+
   """
 
   def available_matchup_sets(rosters) do
